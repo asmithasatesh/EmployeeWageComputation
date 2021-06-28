4d5bff526a8ca93003ea7ff0b11a47cd6109522a
asmithasatesh <asmithasatesh@gmail.com>
2021-06-28 17:42:32 +0530

    Introducing while loop in same program

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,24 +5,25 @@ namespace EmployeeWageComputation
     class Program
     {
         static void Main(string[] args)
-        {
-            int employeeState = 1, partTimeState = 2;
+        {   
             int dailyWage,wageperHour=20, hours=8, partTimeHour=4;
             Random random = new Random();
             int Attendance = random.Next(0, 3);
             Console.WriteLine("Random number is :"+ Attendance);
-
-            if ((partTimeState == Attendance) || (Attendance==employeeState) )
+            switch (Attendance)
             {
-                Console.WriteLine("Employee is Present \n");
+                case 1:
+                    Console.WriteLine("Employee is Present \n");
+                    break;
+                case 2:
+                    Console.WriteLine("Parttimer is Present");
+                    hours = partTimeHour; Attendance = 1;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent \n");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent \n");
 
-            }
-            if (Attendance == 2)
-            { hours = partTimeHour; Attendance = 1; }
             dailyWage = wageperHour * hours*Attendance;
             Console.WriteLine("Employee daily wage for 20/hr is : "+dailyWage);
         }
