f4cb09bd357bbaa5181473bdc456f1950ff4b1fa
asmithasatesh <asmithasatesh@gmail.com>
2021-06-28 16:47:02 +0530

    Making use of Random method to check attendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,20 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int employeeState = 1;
+            Random random = new Random();
+            int Attendance = random.Next(0, 2);
+            Console.WriteLine("Random number is :"+ Attendance);
+
+            if (Attendance==employeeState)
+            {
+                Console.WriteLine("Employee is Present \n");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent \n");
+
+            }
         }
     }
 }
