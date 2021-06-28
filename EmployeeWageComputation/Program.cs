ea1ea4bffc30b3ccfc226bd838b810c8064110fd
asmithasatesh <asmithasatesh@gmail.com>
2021-06-28 17:31:30 +0530

    Compute wage for parttimer

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,13 +6,13 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            int employeeState = 1;
-            int dailyWage, wageperHour=20, hours=8;
+            int employeeState = 1, partTimeState = 2;
+            int dailyWage,wageperHour=20, hours=8, partTimeHour=4;
             Random random = new Random();
-            int Attendance = random.Next(0, 2);
+            int Attendance = random.Next(0, 3);
             Console.WriteLine("Random number is :"+ Attendance);
 
-            if (Attendance==employeeState)
+            if ((partTimeState == Attendance) || (Attendance==employeeState) )
             {
                 Console.WriteLine("Employee is Present \n");
             }
@@ -21,6 +21,8 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee is Absent \n");
 
             }
+            if (Attendance == 2)
+            { hours = partTimeHour; Attendance = 1; }
             dailyWage = wageperHour * hours*Attendance;
             Console.WriteLine("Employee daily wage for 20/hr is : "+dailyWage);
         }
