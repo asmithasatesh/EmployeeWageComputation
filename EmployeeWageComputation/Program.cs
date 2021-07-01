fa440f236ff2064f79443b0f25545053586a519a
asmithasatesh <asmithasatesh@gmail.com>
2021-07-01 19:52:15 +0530

    [Asmitha]Wage Computation for multiple companies

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,44 +6,68 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            ComputeWage();
+            Console.WriteLine("Welcome to employee wage computation");
+
+            //get input from user
+            Console.WriteLine("Enter Company Name");
+            string company=Console.ReadLine();
+            Console.WriteLine("Enter wage per hour");
+            int wageperHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter total number of days");
+            int totalDays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter maximum hours");
+            int totalHour = Convert.ToInt32(Console.ReadLine());
+            ComputeWage(company, wageperHour, totalDays, totalHour);
+
+
         }
-        static void ComputeWage()
+        public static void ComputeWage(string company, int wageperHour, int totalDays, int totalHour)
         {   //Local Variables
-            int dailyWage=0,totalHour=0,wageperHour=20, hours=8, partTimeHour=4,days=0;
+            int workingDays=0, hours=0,dailyWage=0,totalWage=0,workingHour=0;
 
             //Generate a random number in range [0,1,2]
             Random random = new Random();
-            int Attendance = random.Next(0, 3);
-            Console.WriteLine("Random number is :"+ Attendance);
 
             //Using Random methoud output to check attendance
-            switch (Attendance)
+            while (workingDays <= totalDays && hours <= totalHour)
             {
-                case 1:
-                    Console.WriteLine("Employee is Present \n");
-                    break;
-                case 2:
-                    Console.WriteLine("Parttimer is Present \n");
-                    hours = partTimeHour; Attendance = 1;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent \n");
-                    break;
-            }
+                //Calling the next method in Random Class
+                int Attendance = random.Next(0, 3);
+               // Console.WriteLine("Random number is :" + Attendance);
+                switch (Attendance)
+                {
+                    case 1:
+                        //Console.WriteLine("Employee is Present \n");
+                        hours = 8;
+                        break;
+                    case 2:
+                       // Console.WriteLine("Parttimer is Present \n");
+                        hours = 4;
+                        break;
+                    default:
+                       // Console.WriteLine("Employee is Absent \n");
+                        break;
 
-            //Compute Daily wage
-            Console.WriteLine("Employee Daily wage is : " + wageperHour*Attendance*hours);
-            Console.WriteLine("\n");
-
-            //Compute Monthly wage for 100hours (or) 20days
-            while ((totalHour<=100 || days<=20) && Attendance!=0)
-            {
-                dailyWage+= wageperHour * hours;
-                totalHour += hours;
-                days += 1;
+                }
+                //Calculation
+                dailyWage = hours * wageperHour;
+                totalWage += dailyWage;
+                workingHour += hours;
+                if (Attendance != 0)
+                {
+                    workingDays++;
+                }
             }
-            Console.WriteLine("Employee Monthly wage is : " + dailyWage);
+            //Print all Details
+                Console.WriteLine("Company Name:" + company);
+                Console.WriteLine("Working Hours :" + workingHour);
+                Console.WriteLine("Employee Wage Per day :" + dailyWage);
+                Console.WriteLine("Working days in a month :" + workingDays);
+                Console.WriteLine("Employee Wage for 20 working days :" + totalWage);
+                Console.WriteLine("\n");
+
+
+
 
         }
     }
