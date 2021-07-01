6a24285a8625743269d864f08c9d6bb9ed807700
asmithasatesh <asmithasatesh@gmail.com>
2021-07-01 18:56:14 +0530

    [Asmitha]Calculate Daile wage of Employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,11 +7,13 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             int employeeState = 1;
+            //local variables
             int dailyWage, wageperHour=20, hours=8;
             Random random = new Random();
+            //Call Random to generate (0,1)
             int Attendance = random.Next(0, 2);
             Console.WriteLine("Random number is :"+ Attendance);
-
+            //Check if employee present
             if (Attendance==employeeState)
             {
                 Console.WriteLine("Employee is Present \n");
@@ -21,6 +23,7 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee is Absent \n");
 
             }
+            //Calculate daily wage
             dailyWage = wageperHour * hours*Attendance;
             Console.WriteLine("Employee daily wage for 20/hr is : "+dailyWage);
         }
