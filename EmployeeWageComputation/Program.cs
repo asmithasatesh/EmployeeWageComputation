3f313c79bb77c05735a9609a3efcb6b52f88d02a
asmithasatesh <asmithasatesh@gmail.com>
2021-07-01 18:52:29 +0530

    [Asmitha]Checking employee Attendance using Random

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,11 +6,13 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
+            //Local variables
             int employeeState = 1;
             Random random = new Random();
+            //Generate random -> (0,1)
             int Attendance = random.Next(0, 2);
             Console.WriteLine("Random number is :"+ Attendance);
-
+            //Check if employee is present
             if (Attendance==employeeState)
             {
                 Console.WriteLine("Employee is Present \n");
